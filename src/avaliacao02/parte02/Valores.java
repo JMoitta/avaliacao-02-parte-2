a970f4dd34ce5c57b19ca1bcf9ac0f9ecc708e66
joao <joao@UB-L-G50-80>
2018-05-17 21:16:23 -0300

    Primeiro commit

---
+++ src/avaliacao02/parte02/Valores.java
@@ -0,0 +1,72 @@
+package avaliacao02.parte02;
+
+public class Valores implements ValoresITF {
+
+	private int[] inteiros;
+	private int tamanho = 0;
+	
+	public Valores() {
+		inteiros = new int[10];
+	}
+	
+	@Override
+	public boolean ins(int v) {
+		if(size() == 10 || v < 1)return false;
+		inteiros[size()] = v;
+		tamanho++;
+		return true;
+	}
+
+	@Override
+	public int del(int i) {
+		if(size() == 0) return -1;
+		int retorno = inteiros[i];
+		inteiros[i] = 0;
+		for(int j = i; j < size(); j++) {
+			inteiros[j] = inteiros[j+1];
+		}
+		tamanho--;
+		return retorno;
+	}
+
+	@Override
+	public int size() {
+		return tamanho ;
+	}
+
+	@Override
+	public double mean() {
+		if(size() == 0) return 0;
+		double media = 0;
+		for(int i = 0; i < size(); i++) {
+			media += inteiros[i];
+			
+		}
+		return (media / size());
+	}
+
+	@Override
+	public int greater() {
+		if(size() == 0) return -1;
+		int maior = inteiros[0];
+		for (int i = 0; i < size(); i++) {
+			if(inteiros[i] > maior) {
+				maior = inteiros[i];
+			}
+		}
+		return maior;
+	}
+
+	@Override
+	public int lower() {
+		if(size() == 0) return -1;
+		int menor = inteiros[0];
+		for (int i = 0; i < size(); i++) {
+			if(inteiros[i] < menor) {
+				menor = inteiros[i];
+			}
+		}
+		return menor;
+	}
+
+}
---
+++ src/avaliacao02/parte02/ValoresITF.java
@@ -0,0 +1,12 @@
+package avaliacao02.parte02;
+
+public interface ValoresITF {
+
+	boolean ins(int v);//insere um valor
+	int del(int i); // remove/retorna valor de indice i
+	int size(); // retorna quantidade valores armazenados
+	double mean(); // retorna média valores armazenados
+	int greater(); // retorna maior valor armazenado
+	int lower(); //retorna o menor valor armazenado
+	
+}
